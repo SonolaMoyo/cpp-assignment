ff262b3a6e187a35819c2031349f59555bd387a6
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2023-03-19 17:15:39 -0700

    feature to request and save players name

--- 2023Assignment/Mountains.cpp
+++ 2023Assignment/Mountains.cpp
@@ -6,86 +6,51 @@
 #include <string>
 #include <chrono>
 #include <sstream>
-
-Mountains::Mountains(const std::string& filename) {
-        std::srand(std::time(nullptr));
-        std::ifstream infile(filename);
-        std::string line;
-        while (std::getline(infile, line)) {
-            std::istringstream iss(line);
-            std::string range_name;
-            if (!(std::getline(iss, range_name, ','))) {
-                std::cerr << "Error: invalid input line: " << line << std::endl;
-                continue;
-            }
-            std::string mountain_name;
-            while (std::getline(iss, mountain_name, ',')) {
-                Mountain mountain;
-                mountain.name = mountain_name;
-                mountain.range = range_name;
-                m_mountains.push_back(mountain);
-            }
-        }
-        infile.close();
-    }
-
-    void Mountains::print() const {
-        for (const auto& mountain : m_mountains) {
-            std::cout << mountain.name << " in " << mountain.range << std::endl;
-        }
-    }
-
-//Refactored code to use std::rand() instead of rand()
-std::string Mountains::getRandomMountain() const
-{
-    int randomIndex = std::rand() % m_mountains.size();
-    return m_mountains[randomIndex].name;
-}
-
-bool Mountains::checkRange(const std::string& mountain, const std::string& range) const
-{
-    for (const auto& m : m_mountains) {
-        if (m.name == mountain && m.range == range) {
-            return true;
-        }
-    }
-    return false;
-}
+#include <vector>
+#include <utility>
+#include <algorithm>
 
 
-int main()
-{
-    //std::vector<std::string> filenames = {"Alps.txt", "Carpathians.txt", "Icelandic_Highlands.txt", "Pyrenees.txt"};
+int main() {
     Mountains mountains("mountains.csv");
 
     int numQuestions = 0;
     int numCorrect = 0;
 
-    std::cout << "Welcome to the Mountain Range Quiz! Type 'exit' or 'quit' to end the quiz. NB: Answers first letters should be capitalized and spaces replaced with underscore" << std::endl;
+    std::vector<std::pair<std::string, int>> correctAnswers; // to store correctly answered questions and their times
+
+    std::string playerName;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, playerName);
+
+    std::cout << "Welcome, " << playerName << ", to the Mountain Range Quiz! Type 'exit' or 'quit' to end the quiz. NB: Answers first letters should be capitalized and spaces replaced with underscore" << std::endl;
     while (true) {
         std::string mountainName = mountains.getRandomMountain();
 
         std::cout << "Which mountain range is " << mountainName << " in?" << std::endl;
         std::string answer;
-        
+
         auto start = std::chrono::steady_clock::now(); // start timer
 
         std::getline(std::cin, answer);
 
         auto end = std::chrono::steady_clock::now(); // end timer
         auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start).count(); // calculate elapsed time in seconds
-        
+
         if (answer == "exit" || answer == "quit") {
             break;
         }
 
-        if(elapsed > 10){
+        if (elapsed > 10) {
             std::cout << "Too slow! ";
             std::cout << "Incorrect." << std::endl;
         }
         else if (mountains.checkRange(mountainName, answer)) {
             std::cout << "Correct!" << std::endl;
             numCorrect++;
+
+            // add the correctly answered question and its time to the vector
+            correctAnswers.push_back(std::make_pair(mountainName, elapsed));
         }
         else {
             std::cout << "Incorrect." << std::endl;
@@ -94,7 +59,19 @@ int main()
         numQuestions++;
     }
 
-    std::cout << "You scored " << numCorrect << " out of " << numQuestions << " questions." << std::endl;
+    std::cout << playerName << ", you scored " << numCorrect << " out of " << numQuestions << " questions." << std::endl;
+
+    // sort the vector of correctly answered questions based on the time taken
+    std::sort(correctAnswers.begin(), correctAnswers.end(),
+        [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+            return a.second < b.second;
+        });
+
+    // print out the correctly answered questions and their times in order of speed
+    std::cout << "Correctly answered questions (in order of speed):" << std::endl;
+    for (const auto& qa : correctAnswers) {
+        std::cout << qa.first << " (" << qa.second << " seconds)" << std::endl;
+    }
 
     return 0;
 }
--- 2023Assignment/Mountains.h
+++ 2023Assignment/Mountains.h
@@ -2,6 +2,12 @@
 
 #include <vector>
 #include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <chrono>
+#include <sstream>
 
 struct Mountain {
     std::string name;
@@ -13,18 +19,54 @@ class Mountains
 public:
 	//Time complexity: The time complexity of the function is O(n*m), 
 	//where n is the number of files in the input vector and m is the average number of mountains per file.
-    Mountains(const std::string& filename);
+    Mountains(const std::string& filename) {
+        std::srand(std::time(nullptr));
+        std::ifstream infile(filename);
+        std::string line;
+        while (std::getline(infile, line)) {
+            std::istringstream iss(line);
+            std::string range_name;
+            if (!(std::getline(iss, range_name, ','))) {
+                std::cerr << "Error: invalid input line: " << line << std::endl;
+                continue;
+            }
+            std::string mountain_name;
+            while (std::getline(iss, mountain_name, ',')) {
+                Mountain mountain;
+                mountain.name = mountain_name;
+                mountain.range = range_name;
+                m_mountains.push_back(mountain);
+            }
+        }
+        infile.close();
+    }
 
     //Time complexity: The time complexity of the print() function is O(n),
     // where n is the number of mountains in the m_mountains vector
-    void print() const;
+    void print() const {
+        for (const auto& mountain : m_mountains) {
+            std::cout << mountain.name << " in " << mountain.range << std::endl;
+        }
+    }
 
 	//The time complexity of the function is O(1).
-    std::string getRandomMountain() const;
+    std::string getRandomMountain() const
+    {
+    int randomIndex = std::rand() % m_mountains.size();
+    return m_mountains[randomIndex].name;
+    }
 
 	//The time complexity of the function is O(n),
 	//where n is the number of mountains in the m_mountains vector.
-    bool checkRange(const std::string& mountain, const std::string& range) const;
+    bool checkRange(const std::string& mountain, const std::string& range) const
+    {
+    for (const auto& m : m_mountains) {
+        if (m.name == mountain && m.range == range) {
+            return true;
+        }
+    }
+    return false;
+    }
 
 private:
     std::vector<Mountain> m_mountains;
